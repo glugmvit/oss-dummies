1c49ba206690f24c58a63bef71dff4899d995252
prabal gupta <prabalgupta4343@gmail.com>
2018-10-27 14:17:10 +0530

    added factorial.py to py folder
    Signed-off-by: prabal gupta <prabalgupta4343@gmail.com>

---
+++ py/factorial.py
@@ -0,0 +1,11 @@
+num = 7
+factorial = 1
+# check if the number is negative,positive or zero
+if num < 0:
+   print("sorry,factorial does not exist for negative numbers")
+elif num == 0;
+     print("the factorial of 0 is 1")
+else:
+   for i in range (1,num + 1):
+       factorial = factorial*1
+print("the factorial of",num,"is",factorial)
